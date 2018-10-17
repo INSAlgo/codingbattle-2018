cdec61bbf30846d6507079639164aeb4c2c58ca8
Askrenteam <renaud.meurisse@yahoo.fr>
2018-10-17 22:00:51 +0100

    Second exercise in haskell

---
+++ pre-contest/b_the_alchemist/sol/sol.hs
@@ -0,0 +1,11 @@
+main :: IO()
+main = do
+    line <- getLine
+    let li = map (read :: String -> Int) $ words line
+    print ( ans (li!!0) (li!!1) (li!!2) )
+
+ans :: Int -> Int -> Int -> Int
+ans p l g = do
+        length [(x,y)| x<-[0..m], y<-[0..l], x*x+y+x*y==g]
+        -- length [0| x<-[0..p], y<-[0..l], x*x+y+x*y==g]
+            where m = min p $ floor $ sqrt $ fromIntegral g
